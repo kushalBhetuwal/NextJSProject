3d1008bd14ef28b18bd8f9c81ac32c99afa15657
kushal <kushalbhetuwal@gmail.com>
2023-11-21 22:34:31 +1100

    implementing tailwind config and styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <h1 className="h1-bold">Welcome Kushal!!!</h1>
       <h2 className="h2-bold">hello man</h2>
       <h3 className="h3-bold">this is test for next.js</h3>
+      <h4 className="base-bold">Hello I am coming</h4>
     </div>
   )
 }
