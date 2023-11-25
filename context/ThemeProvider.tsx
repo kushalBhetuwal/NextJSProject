b4383afd80f96dac61691b6a1156796debc80c22
kushal <kushalbhetuwal@gmail.com>
2023-11-25 13:19:12 +1100

    implemented themeProvider

---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { useContext, useState, createContext, useEffect } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
+
+  const handleThemeChange = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.classList.add("light");
+    } else {
+      setMode("dark");
+      document.body.classList.add("dark");
+    }
+  };
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within the ThemeProvider");
+  }
+  return context;
+};
