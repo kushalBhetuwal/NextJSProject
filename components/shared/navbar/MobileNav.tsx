ce24992dbfcf4ac9c1ccb1fd2b052bf833aecec8
kushal <kushalbhetuwal@gmail.com>
2024-01-06 02:12:34 +1100

    changed the logo and color

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -69,14 +69,14 @@ const MobileNav = () => {
       >
         <Link href="/" className="flex items-center gap-1">
           <Image
-            src="/assets/images/site-logo.svg"
+            src="/assets/images/site-logo.png"
             width={23}
             height={23}
             alt="DevFlow"
           />
 
           <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
-            Dev <span className="text-primary-500">Overflow</span>
+            Kushal <span className="text-blue-500">Overflow</span>
           </p>
         </Link>
         <div>
